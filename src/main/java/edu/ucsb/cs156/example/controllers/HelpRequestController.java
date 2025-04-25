67fc772a55fc8012cffe69ca3a8d2cab26e6baa3
AyalaWang <148507259+AyalaWang@users.noreply.github.com>
2025-04-24 19:12:09 -0700

    addressed the comments

--- src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/HelpRequestController.java
@@ -25,7 +25,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 
  @Tag(name = "HELPREQUEST")
- @RequestMapping("/api/HELPREQUEST")
+ @RequestMapping("/api/helprequest")
  @RestController
  @Slf4j
 public class HelpRequestController extends ApiController { 
--- src/test/java/edu/ucsb/cs156/example/controllers/HelpRequestControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/HelpRequestControllerTests.java
@@ -53,31 +53,31 @@ public class HelpRequestControllerTests extends ControllerTestCase {
 
     @Test
         public void logged_out_users_cannot_get_all() throws Exception {
-                mockMvc.perform(get("/api/HELPREQUEST/all"))
+                mockMvc.perform(get("/api/helprequest/all"))
                                 .andExpect(status().is(403)); // logged out users can't get all
         }
 
         @WithMockUser(roles = { "USER" })
         @Test
         public void logged_in_users_can_get_all() throws Exception {
-                mockMvc.perform(get("/api/HELPREQUEST/all"))
+                mockMvc.perform(get("/api/helprequest/all"))
                                 .andExpect(status().is(200)); // logged
         }
         @Test
         public void logged_out_users_cannot_post() throws Exception {
-                mockMvc.perform(post("/api/HELPREQUEST/post"))
+                mockMvc.perform(post("/api/helprequest/post"))
                                 .andExpect(status().is(403));
         }
         @WithMockUser(roles = { "USER" })
         @Test
         public void logged_in_regular_users_cannot_post() throws Exception {
-                mockMvc.perform(post("/api/HELPREQUEST/post"))
+                mockMvc.perform(post("/api/helprequest/post"))
                                 .andExpect(status().is(403)); // only admins can post
         }
 
         @WithMockUser(roles = { "USER" })
         @Test
-        public void logged_in_user_can_get_all_ucsbdates() throws Exception {
+        public void logged_in_user_can_get_all_helprequests() throws Exception {
 
                 // arrange
                 LocalDateTime ldt1 = LocalDateTime.parse("2022-01-03T00:00:00");
@@ -110,7 +110,7 @@ public class HelpRequestControllerTests extends ControllerTestCase {
                 when(helpRequestRepository.findAll()).thenReturn(expectedHelpRequests);
 
                 // act
-                MvcResult response = mockMvc.perform(get("/api/HELPREQUEST/all"))
+                MvcResult response = mockMvc.perform(get("/api/helprequest/all"))
                                 .andExpect(status().isOk()).andReturn();
 
                 // assert
@@ -141,7 +141,7 @@ public class HelpRequestControllerTests extends ControllerTestCase {
 
                 // act
                 MvcResult response = mockMvc.perform(
-                                post("/api/HELPREQUEST/post?requesterEmail=wangs557@gmail.com&teamId=team02&tableOrBreakoutRoom=room2&requestTime=2022-01-03T00:00:00&explanation=testing&solved=true")
+                                post("/api/helprequest/post?requesterEmail=wangs557@gmail.com&teamId=team02&tableOrBreakoutRoom=room2&requestTime=2022-01-03T00:00:00&explanation=testing&solved=true")
                                                 .with(csrf()))
                                 .andExpect(status().isOk()).andReturn();
 
